43038ce1e8162397ffb11f50c32c65c60f96f1b5
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-12 21:14:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,4 +51,4 @@ wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/down
 #java -jar paper-1.16.5-790.jar
 tmux new-session -d -s my_session 'playit'
 #playit
-tar -czf past.tar.gz ~
+tar -czf past.tar.gz ~/
