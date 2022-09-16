c11b0af0fdad8a0dc15c0014dea70f744e8f9a21
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-16 09:16:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -57,8 +57,8 @@ wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/down
 #  sleep 2
 #done
 curl -H "Content-Type: application/json" -d '{"username": "test", "content": "Configure Playit"}' "https://ptb.discord.com/api/webhooks/1012830182882685140/coAa8BUhkJJc9EHPAanJ2IECPG9Podh7H3J3cBZPF2_sRqQAOKH-HuKEKuqxr6rBInEC"
-timeout 180s playit
-sleep 30
+timeout 30s playit
+sleep 3
 #playit
 echo "doing backup"
 cd /home/runner
