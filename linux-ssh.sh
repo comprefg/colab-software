b85893553d7a26823fbafdc8a153e012f6245b74
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-16 08:31:02 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -18,9 +18,9 @@ fi
 #sudo mv /home/runner/past.tar.gz /home/pabloesgod/
 echo "moving"
 #cd /
-#sudo mv /home/runner/past.tar.gz /
+sudo mv /home/runner/past.tar.gz /
 echo "extracting"
-#sudo tar -xf /past.tar.gz
+sudo tar -xf /past.tar.gz
 echo "done extracting"
 ln -s ~/playit /etc/playit 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
