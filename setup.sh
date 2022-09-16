d8b3ca64f4a0c37bb7ca831f4aade8f95b87efff
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-16 10:06:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,9 +16,9 @@ fi
 #if [1 -eq 1]; then
 sudo su pablogod
 cd /home/pablogod
-sudo mkdir /etc/playit
+#sudo mkdir /etc/playit
 sudo mkdir /home/pablogod/playit
-sudo ln -s /home/pablogod/playit /etc/playit 
+sudo ln -s /home/pablogod/playit /etc/ 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
 sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
 sudo apt -qq update
