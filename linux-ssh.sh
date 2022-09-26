4661bfc3a95f567befd2db68a22b1fed48aea060
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-26 18:35:19 -0500

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -54,6 +54,7 @@ wget -q https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/down
 
 
 #java -jar paper-1.16.5-790.jar
+pip install selenium
 
 cd /home/pablogod/backup
 sudo timeout 600s playit 2>&1 | tee /home/pablogod/backup/somefile.txt &
