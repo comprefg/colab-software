96421f4133ed30e39355593b14b7986febe03b08
comprefg <39472523+comprefg@users.noreply.github.com>
2022-09-26 16:08:07 -0500

    Update playit.py

--- playit.py
+++ playit.py
@@ -21,3 +21,43 @@ print(resp.status_code)
 
 print(data)
 print(last_line)
+#####part 2
+import os
+os.system("pip3 install selenium")
+os.system("sudo wget https://chromedriver.storage.googleapis.com/106.0.5249.21/chromedriver_linux64.zip")
+os.system("sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+os.system("sudo apt install unzip")
+os.system("sudo pabloesgod")
+os.system("sudo unzip chromedriver_linux64.zip")
+os.system("sudo dpkg -i google-chrome-stable_current_amd64.deb")
+os.system("sudo pip3 install webdriver_manager")
+os.system("sudo pip3 install webdriver-manager")
+os.system("sudo pip3 install --upgrade requests")
+
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+#service = webdriver.Chrome(options=options,executable_path=ChromeDriverManager().install())
+#driver = webdriver.Chrome(options=options,service=service)
+def fill(what,where):
+    driver.find_element(By.NAME, where).send_keys(what)
+    pass
+
+driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
+driver.get("https://playit.gg/claim/cbd7562ebb")
+fill("rfgarcia2023@gmail.com","email")
+fill("Er12345re","password")
+driver.execute_script('document.getElementsByClassName("rJHtO")[0].getElementsByTagName("input")[2].click()')
+time.sleep(5)
+driver.execute_script('document.getElementsByTagName("button")[0].click()')
+time.sleep(10)
