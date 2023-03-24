a37b4f073cf5e4f435873f1f38c503ef18839d07
comprefg <39472523+comprefg@users.noreply.github.com>
2023-03-23 19:12:32 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -87,6 +87,5 @@ python3 playit.py
 
 #sudo playit
 echo "doing backup"
-cd /home/runner
 #sudo mv /var/lib/pufferpanel /home/pablogod/backup/pufferpanel
 #sudo tar -zcf past.tar.gz /home/pablogod/backup/
